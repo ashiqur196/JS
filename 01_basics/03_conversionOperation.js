34573f8ea811c1cd226fc5705d4916021d2bdcb7
ASHIQUR RUBBIN TALEB AYON <67256635+ashiqur196@users.noreply.github.com>
2024-08-15 08:22:32 +0000

    convertion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+ let score = true
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log( valueInNumber); 
+
+
+ // "33" => 33
+ // "33abc" => NaN
+ // null => 0 
+ // true => 1; false => 0
+
+ let isLoggedIn = "Ayon"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //onsole.log(booleanIsLoggedIn);
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "Ayon" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
+ 
